fdc477ff864f501fed6b160e4fc3efb810935620
HiperFelps <162184664+HiperFelps@users.noreply.github.com>
2025-04-10 21:35:53 -0300

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Convidados</title>
+</head>
+<body>
+    <h1>Adicionar Convidados</h1>
+    <form action="index3.php" method="post">
+        <label>Insira o nome dos seus convidados</label><br>
+        <input type="text" size="80" name="nome"><br><br>
+        <input type="submit" name="adicionar" value="Adicionar"><br><br>
+        <input type="submit" name="remover" value="Remover"><br><br>
+    </form>
+</body>
+</html>
+
+<?php
+
+    session_start();
+
+    if (!isset($_SESSION["convidados"])){
+        $_SESSION["convidados"] = [];
+    }
+    if(isset($_POST["adicionar"])){
+        $nome = $_POST["nome"];
+        array_push($_SESSION["convidados"], $nome);
+
+    foreach ($_SESSION["convidados"] as $i => $v) {
+            echo "[$i] - $v <br>";
+        }
+    }
+    if(isset($_POST["remover"])){
+        array_pop($_SESSION["convidados"]);
+
+        foreach ($_SESSION["convidados"] as $i => $v) {
+            echo "[$i] - $v <br>";
+        }
+    }
+
+
+?>
